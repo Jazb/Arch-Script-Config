8b2c2da7e09320250bde71cbd2b604b535050d8a
Jesús Zuleta <jesuszuleta@gmail.com>
2015-01-14 00:28:00 -0430

    Borrada linea de entrada en la jaula  [chroot]

--- steps/base.sh
+++ steps/base.sh
@@ -3,5 +3,4 @@
 pacstrap /mnt abs base base-devel grub os-prober networkmanager xf86-input-synaptics iw wireless_tools wpa_actiond wpa_supplicant dialog alsa-utils espeakup brltty parted btrfs-progs openssh net-tools ntp git vim fish tmux
 genfstab -p -U /mnt >> /mnt/etc/fstab
 
-arch-chroot /mnt
 
