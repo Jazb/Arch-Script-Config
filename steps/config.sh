d479f7f2471b26634b0605f4e3001dcfccc0a3ad
Jesús Zuleta <jesuszuleta@gmail.com>
2015-01-14 19:48:51 -0430

    Utilizar de forma completa el sharedfuncs de AUI
    remplazar de forma absoluta las funciones implementadas por el guion de helmuthdu/aui

--- steps/config.sh
+++ steps/config.sh
@@ -11,6 +11,7 @@ ZONE="America"
 SUBZONE="Caracas"
 LOCALE_UTF8="es_VE.UTF-8"
 
+## Configuraciones basicas
 arch_chroot "echo '${host_name}'>> /etc/hostname"
 arch_chroot "sed -i '/127.0.0.1/s/$/ '${host_name}'/' /etc/hosts"
 arch_chroot "sed -i '/::1/s/$/ '${host_name}'/' /etc/hosts"
@@ -20,3 +21,15 @@ arch_chroot "echo LANG='${LOCALE_UTF8}' >> /etc/locale.conf"
 arch_chroot "echo 'KEYMAP=es' >> /etc/vconsole.conf"
 arch_chroot "sed -i '/'${LOCALE_UTF8}'/s/^#//' /etc/locale.gen"
 arch_chroot "locale-gen"
+
+## Configuraciones de Kernel, init y Grub
+arch_chroot "sed -i '/^MODULES/s/""/"lzo tuxonice_compress tuxonice_swap tuxonice_userui"/' /etc/mkinitcpio.conf"
+arch_chroot "sed -i '/^HOOK/s/block/block keymap encrypt/' /etc/mkinitcpio.conf"
+arch_chroot "sed -i '/^HOOK/s/filesystems/lvm2 userui resume filesystems/' /etc/mkinitcpio.conf"
+arch_chroot "mkinitcpio -p linux-pf"
+
+sed -i 's/#[multinit/[multibit]'
+sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=1/' /etc/default/grub
+
+arch_chroot "grub-install --target=i386-pc --recheck --debug ${BOOT_MOUNTPOINT}"
+arch_chroot "grub-mkconfig -o /boot/grub/grub.cfg"
